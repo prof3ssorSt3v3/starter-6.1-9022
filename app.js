200d6af74315fef681876806b093541f5f35f032
Prof3ssorSt3v3 <griffis@algonquincollege.com>
2022-02-14 11:55:44 -0500

    starter code

---
+++ app.js
@@ -0,0 +1,18 @@
+const APP = {
+  defaultImg: 'https://via.placeholder.com/300x100/639/fff',
+  fullImg: 'nasa-Q1p7bh3SHj8-unsplash.jpeg',
+  init: () => {
+    //start building the list
+  },
+  buildList: () => {
+    //add the list items with text and default image
+    //when done, add the images
+  },
+  addImages: () => {
+    //add an image source for each image
+    //when done, tell the user
+  },
+  message: (msg) => {
+    //tell the user that you are done.
+  },
+};
